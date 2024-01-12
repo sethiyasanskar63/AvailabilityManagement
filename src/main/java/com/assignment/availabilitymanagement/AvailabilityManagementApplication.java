dc460b8dee4e5cceff97dc1b83b0186e38dc3cd0
Sanskar Sethiya <sethiyasanskar63@gmail.com>
2024-01-12 11:46:14 +0530

    Initial project setup.

---
+++ src/main/java/com/assignment/availabilitymanagement/AvailabilityManagementApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.availabilitymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvailabilityManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvailabilityManagementApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/assignment/availabilitymanagement/entity/Resort.java
@@ -0,0 +1,4 @@
+package com.assignment.availabilitymanagement.entity;
+
+public class Resort {
+}
